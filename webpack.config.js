fb325d4c81368440e9aaa792f5aa45fd13d43534
ME-LocalBody <localbody@yandex.ru>
2021-06-30 14:55:02 +0300

    3th

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,16 @@
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin'); // добавляет нужные поля в HTML файл
 
+const NODE_ENV = process.env.NODE_ENV;
+
 module.exports = {
-    mode: 'development',
+    mode: NODE_ENV || 'development',
     entry: path.resolve(__dirname, 'src/index.js'),
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist' ),
     },
-    watch: true,
+    watch: NODE_ENV === 'development',
     watchOptions: {
         ignored: /node_modules/,
         poll: 1000,
@@ -55,5 +57,6 @@ module.exports = {
         open: true,
         hot: true,
         historyApiFallback: true,
-    }
+    },
+    devtool: NODE_ENV ===  'development' ? 'source-map' : false,
 }
