25b6c83d940a5f19b3dc7cb2f31ae90ebc6fbb75
ME-LocalBody <localbody@yandex.ru>
2021-07-16 09:04:44 +0300

    add arrows

--- src/client/ClientWorld.js
+++ src/client/ClientWorld.js
@@ -1,62 +1,60 @@
 import PositionedObject from '../common/PositionedObject';
 import ClientCell from './ClientCell';
 
-class ClientWorld extends PositionedObject{
-    constructor(game, engine, levelCfg) {
-        super();
+class ClientWorld extends PositionedObject {
+  constructor(game, engine, levelCfg) {
+    super();
 
-        const worldHeight = levelCfg.map.length;
-        const worldWidth = levelCfg.map[0].length;
-        const cellSize = engine.canvas.height / levelCfg.camera.height;
+    const worldHeight = levelCfg.map.length;
+    const worldWidth = levelCfg.map[0].length;
+    const cellSize = engine.canvas.height / levelCfg.camera.height;
 
-        Object.assign(this, {
-            game,
-            engine,
-            levelCfg,
-            height: worldHeight * cellSize,
-            width: worldWidth * cellSize,
-            worldHeight,
-            worldWidth,
-            cellWidth: cellSize,
-            cellHeight: cellSize,
-            map: [],
+    Object.assign(this, {
+      game,
+      engine,
+      levelCfg,
+      height: worldHeight * cellSize,
+      width: worldWidth * cellSize,
+      worldHeight,
+      worldWidth,
+      cellWidth: cellSize,
+      cellHeight: cellSize,
+      map: [],
+    });
+  }
+
+  init() {
+    const { levelCfg, map, worldWidth, worldHeight } = this;
+
+    for (let row = 0; row < worldHeight; row++) {
+      for (let col = 0; col < worldWidth; col++) {
+        if (!map[row]) {
+          map[row] = [];
+        }
+
+        map[row][col] = new ClientCell({
+          world: this,
+          cellCol: col,
+          cellRow: row,
+          cellCfg: levelCfg.map[row][col],
         });
+      }
     }
+  }
 
-    init() {
+  render(time) {
+    const { map, worldWidth, worldHeight } = this;
 
-        const { levelCfg, map, worldWidth, worldHeight } = this;
-
-        for ( let row = 0; row < worldHeight; row++ ) {
-            for ( let col = 0; col< worldWidth; col++ ) {
-                if (!map[row]) {
-                    map[row] = [];
-                }
-
-                map[row][col] = new ClientCell({
-                    world: this,
-                    cellCol: col,
-                    cellRow: row,
-                    cellCfg: levelCfg.map[row][col],
-                })
-            }
-        }
-
+    for (let row = 0; row < worldHeight; row++) {
+      for (let col = 0; col < worldWidth; col++) {
+        map[row][col].render(time);
+      }
     }
+  }
 
-    render(time) {
-        const {map, worldWidth, worldHeight} = this;
-
-        for (let row =0; row < worldHeight; row++) {
-            for (let col = 0; col < worldWidth; col++) {
-                map[row][col].render(time);
-            }
-        }
-    }
-
-    cellAt(col, row) {
-        return this.map[row] && this.map[row][col];
-    }
+  cellAt(col, row) {
+    return this.map[row] && this.map[row][col];
+  }
 }
 
 export default ClientWorld;
